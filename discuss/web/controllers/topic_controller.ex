ade859e63e3aa9f3cffe717c55d6fa851a1b5dfa
Carson McKinstry <carson.mckinstry@gmail.com>
2017-10-10 16:24:32 -0500

    Routes now restricted by user_id

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -1,6 +1,16 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
+
   alias Discuss.Topic
+
+  plug Discuss.Plugs.RequireAuth when action in [
+    :new,
+    :create,
+    :edit,
+    :update,
+    :delete
+  ]
+  plug :check_topic_owner when action in [:update, :edit, :delete]
 
   def index(conn, _params) do
     topics = Repo.all(Topic)
@@ -13,7 +23,10 @@ defmodule Discuss.TopicController do
   end
 
   def create(conn,  %{"topic" => topic}) do
-    changeset = Topic.changeset(%Topic{}, topic)
+
+    changeset = conn.assigns.user
+      |> build_assoc(:topics)
+      |> Topic.changeset(topic)
     
     case Repo.insert(changeset) do
       {:ok, _topic} ->
@@ -53,6 +66,19 @@ defmodule Discuss.TopicController do
     conn
     |> put_flash(:info, "Topic deleted")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You cannot edit that")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
   end
 
 end
